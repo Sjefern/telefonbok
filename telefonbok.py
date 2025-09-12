5c37ce1474fd3d16980722eb9ca408b28b6f6eec
Sjefern <sebastiankjos08@gmail.com>
2025-09-12 15:08:38 +0200

    Vi lager tom liste, lager to ordbøker til person 1 og 2, legger til begge personene i listen.

---
+++ telefonbok.py
@@ -0,0 +1,14 @@
+#telefon bok
+
+# Lager en tom telefonbok liste
+telefonbok = []
+
+# Lager ordbøker med to personer og tlf nummeret
+Person1 = {"navn":"Sebastian", "nummer": 1479643}
+Person2 = {"navn":"Marcus", "nummer": 14767883}
+
+# legger til begge personene til i listen ved å bruke apend
+telefonbok.append(Person1)
+telefonbok.append(Person2)
+
+
