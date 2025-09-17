9da6109bb318f3151abcc2551292615aee975e2d
Sjefern <sebastiankjos08@gmail.com>
2025-09-17 17:40:21 +0200

    søker etter et navn i telefonboken og viser nummeret hvis det finnes, ellers gir den beskjed om at navnet ikke ble funnet.

--- telefonbok.py
+++ telefonbok.py
@@ -35,3 +35,25 @@ telefonbok.append(ny_persjon)
 
 # viser en melding om at personen ble lagt til.
 print(f"{navn} ble lagt til i telefonboka.")
+
+# lager en funksjon
+def søk():
+    # Ber brukeren skrive inn et navn og gjør det om til små bokstaver for enklere sammenligning
+    søkenavn = input("Skriv inn navnet du vil søke etter: ").lower()
+    # lager et flagg som viser om vi har funnet noen eller ikke
+    funnet = False
+    # går gjennom alle personer i telefonbok-listen
+    for person in telefonbok:
+        # sjekker om navnet til personen er likt søkenavnet
+        if person["navn"].lower() == søkenavn:
+            # skriver ut navn og nummer hvis det er en match
+            print(f"Fant: {person['navn']} - {person['nummer']}")
+            # markerer at vi har funnet en person.
+            funnet = True
+            # stopper løkken når en match er funnet
+            break
+    # sjekker om ingen ble funnet
+    if not funnet:
+        # skriver ut melding hvis ingen ble funnet
+        print("Fant ingen med det navnet.")
+
